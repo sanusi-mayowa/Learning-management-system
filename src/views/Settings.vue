b0eb1debd0b9de62468d287bc06dd97238ea600d
sanusi-mayowa <sanusifaruq007@gmail.com>
2024-07-31 14:39:07 +0100

    update

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -4,7 +4,8 @@
             <v-col col="12" md="5">
                 <v-card flat>
                     <div class="d-flex justify-space-between align-center px-2 py-2 mx-2 my-2" v-for="setting in settings" :key="setting.id" style="cursor: pointer;">
-                        <div>
+                        <div class="d-flex align-center">
+                            <img :src="setting.icon" alt="">
                             <p>{{ setting.text }}</p>
                         </div>
                         <div>
@@ -22,12 +23,13 @@ export default{
     data(){
         return{
             settings:[
-                { icon: '/assets/home 2.png', text: 'Notifications', backward:'/assets/Backward.png'},
-                { icon: '/assets/home 2.png', text: 'Notifications', backward:'/assets/Backward.png'},
-                { icon: '/assets/home 2.png', text: 'Notifications', backward:'/assets/Backward.png'},
-                { icon: '/assets/home 2.png', text: 'Notifications', backward:'/assets/Backward.png'},
-                { icon: '/assets/home 2.png', text: 'Notifications', backward:'/assets/Backward.png'},
-                { icon: '/assets/home 2.png', text: 'Notifications', backward:'/assets/Backward.png'},
+                { icon: '/assets/bell.png', text: 'Notifications', backward:'/assets/Backward.png'},
+                { icon: '/assets/password.png', text: 'Change Password', backward:'/assets/Backward.png'},
+                { icon: '/assets/security.png', text: 'Security', backward:'/assets/Backward.png'},
+                { icon: '/assets/friends.png', text: 'Friends', backward:'/assets/Backward.png'},
+                { icon: '/assets/home 2.png', text: 'Enable Two Step Verification  ', backward:'/assets/Backward.png'},
+                { icon: '/assets/home 2.png', text: 'Display and languages', backward:'/assets/Backward.png'},
+                { icon: '/assets/home 2.png', text: 'Help', backward:'/assets/Backward.png'},
             ]
         }
     }
